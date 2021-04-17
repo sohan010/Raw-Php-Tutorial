5ec877e275c70534f9c965bb0eb1191fedb0e509
sohan010 <sopnilsohan03@gmail.com>
2021-04-18 00:05:48 +0600

    Switch Case Statement

---
+++ switch-case.php
@@ -0,0 +1,39 @@
+<?php
+
+
+$case = 44;
+
+
+switch ($case) {
+	case 1:
+		echo "Saturday";
+		break;
+
+	case 2:
+		echo "Sunday";
+		break;
+
+	case 3:
+		echo "Monday";
+		break;
+	
+	case 4:
+		echo "Tuesday";
+		break;
+
+	case 5:
+		echo "Wednesday";
+		break;
+
+	case 6:
+		echo "Thursday";
+		break;
+
+	case 7:
+		echo "Friday";
+		break;
+			
+	default:
+		echo "Unkown Input";
+		break;
+}
