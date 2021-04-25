0bfdae25c5cc4004fd9a2ed70b7c0766c29b62af
sohan010 <sopnilsohan03@gmail.com>
2021-04-25 09:56:43 +0600

    Array

---
+++ array.php
@@ -0,0 +1,42 @@
+<?php
+
+
+$number = array(10,20,30,40,50,60,70,80);
+$name = ['Sohan','Sawon','Panna'];
+
+// $arrayName = array('11' => "PHP", '5'=>'Java', '8'=>'C#' );
+$length = count($number);
+
+
+for ($i=0; $i < $length; $i++) { 
+	echo $number[$i] . "<br>";
+}
+
+// $colors[0]= 'green';
+// $colors[1]= 'yellow';
+// $colors[2]= 'pink';
+// $colors[3]= 'red';
+
+// echo $number[1]."<br>";
+// echo $name[1];
+
+// echo "<pre>";
+// 	print_r($name);
+// echo "</pre>";
+
+
+
+// echo "<pre>";
+// 	print_r($arrayName);
+// echo "</pre>";
+
+
+
+// echo "<pre>";
+// 	print_r($colors);
+// echo "</pre>";
+
+
+
+
+
