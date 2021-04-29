03bbcb0ca31f579caecca6955c7622dcd848754d
sohan010 <sopnilsohan03@gmail.com>
2021-04-29 09:11:52 +0600

    Array Pop and Push

---
+++ Array_Pop_Push.php
@@ -0,0 +1,12 @@
+<?php
+
+
+$friends = ["Anup","Raj","Kamrul","Badhon","Ridoy"];
+
+//array_pop($friends);
+array_push($friends,"Monir","Reezvi");
+
+
+echo "<pre>";
+    print_r($friends);
+echo "</pre>";
