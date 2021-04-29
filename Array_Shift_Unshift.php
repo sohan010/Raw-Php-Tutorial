e114b32782eecf8c959631e693b6db8ad6158fbb
sohan010 <sopnilsohan03@gmail.com>
2021-04-29 09:16:58 +0600

    Array Shift Unshift function

---
+++ Array_Shift_Unshift.php
@@ -0,0 +1,12 @@
+<?php
+
+
+$friends = ["Anup","Raj","Kamrul","Badhon","Ridoy"];
+
+array_unshift($friends,"Popy","Bristy");
+//array_shift($friends);
+
+
+echo "<pre>";
+    print_r($friends);
+echo "</pre>";
