c9ae875e722979180cba84411d47fcab91469d0b
sohan010 <sopnilsohan03@gmail.com>
2021-04-30 22:26:11 +0600

    Array keys functions

---
+++ Array_Key_Function.php
@@ -0,0 +1,16 @@
+<?php
+
+$colors = ['a'=>'Green','b'=>'White','c'=>'Blue','d'=>'Black','e'=>'Yellow'];
+
+$newArray= key_exists('dffgdfg',$colors);
+
+
+if($newArray){
+    echo "Key Found";
+}else{
+    echo "Key Not Found";
+}
+
+// echo "<pre>";
+//     print_r($newArray);
+// echo "</pre>";
